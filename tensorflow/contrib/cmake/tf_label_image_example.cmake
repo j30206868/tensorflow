8ff354617b08028aad1f024735db7edd1e4ebb64
j30206868 <jerry88114561@gmail.com>
2017-09-19 23:30:43 +0800

    Update tf_label_image_example.cmake

--- tensorflow/contrib/cmake/tf_label_image_example.cmake
+++ tensorflow/contrib/cmake/tf_label_image_example.cmake
@@ -31,6 +31,7 @@ add_executable(tf_label_image_example
 
 target_link_libraries(tf_label_image_example PUBLIC
     tf_protos_cc
+    tf_python_protos_cc
     ${tf_core_gpu_kernels_lib}
     ${tensorflow_EXTERNAL_LIBRARIES}
 )
